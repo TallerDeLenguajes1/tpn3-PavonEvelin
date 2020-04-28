d135dfbffc909bf66fb4a41a1cf280693cb32b09
PavonEvelin <evelinluz17@gmail.com>
2020-04-28 18:51:05 -0300

    Punto 4 corregido

--- punto4.cpp
+++ punto4.cpp
@@ -23,26 +23,31 @@ typedef struct Cliente {
 char *TiposProductos[]={"Galletas","Snack","Cigarrillos","Caramelos","Bebidas"};
 
 void mostrar(cliente *Clientes, int cantClientes);
-float costoTotal(cliente *Cliente);
+float costoTotal(int cantidad, float precioUnitario);
 
 int main(void) {
   srand(time(NULL));
   char nombre[MAX];
-  int cantClientes, aleatorio, tamanio;
+  int cantClientes, aleatorio, tamanio, tamanioNom;
   cliente *Clientes;
   producto *Productos;
 
   printf("Por favor, Ingrese la cantidad de clientes (no debe ser mayor a 5)\n");
   scanf("%d", &cantClientes);
-
+  getchar();
+  printf("cantidad de clientes%d\n", cantClientes);
   Clientes = (cliente *)malloc(sizeof(cliente) * cantClientes);//reservo memoria dinamica para la cantidad de clientes ingresada por el usuario.
 
   //CARGA DE CADA CLIENTE 
-  puts("Ingrese el nombre de los clientes a continuación separado por espacios");
+  
   for(int i=0; i<cantClientes;i++){
+    printf("Ingrese el nombre del cliente\n");
     gets(nombre);
-    Clientes[i].ClienteID = i; 
-    strcpy(Clientes[i].NombreCliente, nombre);
+    //scanf("%s", Clientes[i].NombreCliente);
+    Clientes[i].ClienteID = i+1;
+    tamanioNom = strlen(nombre);
+    Clientes[i].NombreCliente = (char *)malloc(sizeof(tamanioNom)); 
+    strcpy(Clientes[i].NombreCliente,nombre);
     Clientes[i].CantidadProductosAPedir = rand()%10 + 1;
     Clientes[i].Productos = (producto *)malloc(sizeof(producto)*Clientes[i].CantidadProductosAPedir);
     for(int j=0; j<Clientes[i].CantidadProductosAPedir; j++){
@@ -56,22 +61,22 @@ int main(void) {
     }
   }
 
+  mostrar(Clientes,cantClientes);
+
   return 0;
 }
 
-float costoTotal(cliente *Clientes){
-  cliente auxCliente;
+float costoTotal(int cantidad, float precioUnitario){
   float total;
-  total = auxCliente.Productos->PrecioUnitario * auxCliente.Productos->Cantidad;
-  auxCliente = *Clientes;
+  total = cantidad * precioUnitario;
   return total;
 }
 
 void mostrar(cliente *Clientes, int cantClientes){
   float suma;
-  printf("LISTA DE CLIENTES\n\n");
+  printf("################ LISTA DE CLIENTES ###############\n\n");
   for(int i=0; i<cantClientes; i++){
-    printf("Cliente N°%d\n", i+1);
+    printf("\n######### Cliente N°%d ##########\n", i+1);
     printf("ID: %d\n", Clientes[i].ClienteID);
     printf("Nombre: %s\n", Clientes[i].NombreCliente);
     printf("Cantidad de productos pedidos: %d\n", Clientes[i].CantidadProductosAPedir);
@@ -81,10 +86,10 @@ void mostrar(cliente *Clientes, int cantClientes){
       printf("ID: %d\n",(Clientes[i].Productos[j]).ProductoID);
       printf("Tipo de producto: %s\n",(Clientes[i].Productos[j]).TipoProducto);
       printf("Cantidad de %s: %d\n",(Clientes[i].Productos[j]).TipoProducto,(Clientes[i].Productos[j]).Cantidad);
-      printf("Precio unitario: %.2f\n", (Clientes[i].Productos[j]).PrecioUnitario);
-      printf("Costo total del producto: %f\n", costoTotal(Clientes));
-      suma+= costoTotal(Clientes);
+      printf("Precio unitario: $%.2f\n", (Clientes[i].Productos[j]).PrecioUnitario);
+      printf("Costo total del producto: $%.2f\n", costoTotal(Clientes[i].Productos[j].Cantidad,Clientes[i].Productos[j].PrecioUnitario));
+      suma+= costoTotal(Clientes[i].Productos[j].Cantidad,Clientes[i].Productos[j].PrecioUnitario);
     }
-    printf("El total a pagar del cliente es: %.2f", suma);
+    printf("El total a pagar de sus productos es: $%.2f\n\n", suma);
   }
 }
